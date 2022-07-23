474853e94148962caaf70bb2e011095e399833f2
Kiran <Kiran2x15@gmail.com>
2022-07-23 14:17:06 -0700

    redid landing

--- rto/src/containers/Landing/Landing.jsx
+++ rto/src/containers/Landing/Landing.jsx
@@ -6,12 +6,18 @@ import landingPic from "./landing.jpg";
 const Landing = () => {
   return (
     <div className="wrapper">
-      <img src={landingPic} class="img-fluid" alt="Responsive image"></img>
-
+      {/* <img src={landingPic} class="img-fluid" alt="logo"></img>
       <div className="texts">
         <div className="title"> Pawfect Finder</div>
         <Link to="/login" className="link">
           Find your lost pet{" "}
+        </Link>
+      </div> */}
+      <div className="texts">
+        <div className="title"> Pawfect Finder</div>
+        <div className="subtitle"> Make your pet safe and happy</div>
+        <Link to="/login" className="link">
+          Start Now →
         </Link>
       </div>
     </div>
