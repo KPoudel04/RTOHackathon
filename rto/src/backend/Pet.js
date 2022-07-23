d32446277500fcf5736bb6b8627224749f5686e1
Mitchell Salomon <mitchell_salomon@brown.edu>
2022-07-23 14:23:16 -0400

    pet persistence

--- rto/src/backend/Pet.js
+++ rto/src/backend/Pet.js
@@ -1,21 +1,43 @@
+import { collection, addDoc, Timestamp } from "firebase/firestore";
+import { db } from './Firebase';
 export class Pet {
-  constructor(name, sex, species, breed, weight, height, color, allergies, other) {
+  constructor(name, sex, species, breed, weight, height, color, allergies, other, location) {
     this.name = name;
     this.sex = sex;
     this.species = species;
-    this.breed = breed;
     this.weight = weight;
     this.height = height;
     this.color = color;
-    this.allergies = allergies;
-    this.other = other;
+    if (breed)
+      this.breed = breed;
+    if (allergies && allergies.length !== 0)
+      this.allergies = allergies;
+    if (other)
+      this.other = other;
+    if (location) 
+      this.location = location;
+    
+    this.lastModified = Timestamp.now();
   }
 
   /**
-   * Persists this pet to firestore
+   * Persist a pet to firestore
+   * @returns {Promise<Result<Pet>>} a result containing the persisted pet
    */
-  persist() {
-    // TODO
+  async persist() {
+    try {
+      const docRef = await addDoc(collection(db, "pets"), {...this});
+      console.log("Document written with ID: ", docRef.id);
+      return {
+        val: this
+      }
+    } catch (e) {
+      console.error("Error adding document: ", e);
+      return {
+        val: null,
+        status: "Not a valid document"
+      }
+    }
   }
 
   uploadPicture() {
