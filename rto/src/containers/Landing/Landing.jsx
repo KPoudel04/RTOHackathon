79b07d3144f11a8c79b3f9cd806388ba6217978a
anuzza <anuja3580@gmail.com>
2022-07-23 00:52:12 -0500

    refactored landingPage

--- rto/src/containers/Landing/Landing.jsx
+++ rto/src/containers/Landing/Landing.jsx
@@ -1,32 +1,31 @@
-import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
-import "./Landing.css"
-import landingPic from './landing.png'
+import React from "react";
+import { Container } from "react-bootstrap";
+import "./Landing.css";
+import landingPic from "./landing.png";
 
 const Landing = () => {
   return (
-    <div>
-      <Container>
-        <div className="centered">
-          <div className="row">
-            <div className="column">
-              <img src={landingPic} class="img-fluid" alt="Responsive image">
-              </img>
-            </div>
-
-            <div className="column">
-              <div className="landing-text">
-                <h1 > Pawfect Finder</h1>
-                <h2>Find your perfect pet</h2>
-              </div>
-            </div>
+    <Container>
+      <div className="centered">
+        <div className="row">
+          <div className="column">
+            <img
+              src={landingPic}
+              class="img-fluid"
+              alt="Responsive image"
+            ></img>
           </div>
 
+          <div className="column">
+            <div className="landing-text">
+              <h1> Pawfect Finder</h1>
+              <h2>Find your perfect pet</h2>
+            </div>
+          </div>
         </div>
-
-      </Container>
-    </div >
-  )
-}
+      </div>
+    </Container>
+  );
+};
 
 export default Landing;
