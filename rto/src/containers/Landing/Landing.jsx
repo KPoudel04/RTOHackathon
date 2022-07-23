4a3da19b79a6cfd2e03e072a68dc703504b43a90
Kiran <Kiran2x15@gmail.com>
2022-07-22 22:42:17 -0700

    Worked on landing

--- rto/src/containers/Landing/Landing.jsx
+++ rto/src/containers/Landing/Landing.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
 import "./Landing.css"
+import landingPic from './landing.png'
 
 const Landing = () => {
   return (
-    <div>Landing</div>
+    <div>
+      <Container>
+        <div className="centered">
+          <div className="row">
+            <div className="column">
+              <img src={landingPic} class="img-fluid" alt="Responsive image">
+              </img>
+            </div>
+
+            <div className="column">
+              <div className="landing-text">
+                <h1 > Pawfect Finder</h1>
+                <h2>Find your perfect pet</h2>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </Container>
+    </div >
   )
 }
 
