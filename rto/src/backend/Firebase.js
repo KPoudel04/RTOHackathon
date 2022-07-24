6b311dee4c8ab7315bb041dd1e33ab71d160aae4
Mitchell Salomon <mitchell_salomon@brown.edu>
2022-07-24 12:25:10 -0400

    fix sign up bug

--- rto/src/backend/Firebase.js
+++ rto/src/backend/Firebase.js
@@ -51,12 +51,12 @@ export async function registerUser(name, email, password) {
         // Signed in 
         const userId = userCredential.user.uid;
         const user = new User(userId, name, email);
-        return user.addCoordinates();
-        // return user.persist();
+        // return user.addCoordinates();
+        return user.persist();
       })
-      .then((coordsResult) => {
-        return coordsResult.val.persist();
-      })
+      // .then((coordsResult) => {
+      //   return coordsResult.val.persist();
+      // })
       .then((persistResult) => {
         return {
           val: persistResult.val,
